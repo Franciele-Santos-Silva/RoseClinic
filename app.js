81d6531891e6dea8273ebbfdbd3f1dfcdd416421
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-02-14 19:09:35 -0300

    feat:requisição http trasformada em objeto JS

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const Produtos = require("./models/Produtos");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post("/cadastro", (req, res) => {
   Produtos.create({
     nome: req.body.nome,
     preco: req.body.preco,
     descricao: req.body.descricao,
-  }).then(() => {
-    res.send("Produto cadastrado com sucesso!!!");
-  }).catch((erro) => {
-    res.send("Erro ao cadastrar produto" + erro);
-  });
+  })
+    .then(() => {
+      res.send("Produto cadastrado com sucesso!!!");
+    })
+    .catch((erro) => {
+      res.send("Erro ao cadastrar produto" + erro);
+    });
 });
 
 app.listen(8081, () => {
   console.log("Rodando...");
 });
- 
